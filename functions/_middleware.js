ebd31d97db9c61a832cbada0c2f927056e537701
Neo <neo@quweiceshi.com>
2026-05-21 14:00:36 +0800

    fix: add _middleware.js to serve ads.txt via Cloudflare Pages Function

---
+++ functions/_middleware.js
@@ -0,0 +1,12 @@
+export const onRequest = async (context) => {
+  const url = new URL(context.request.url);
+  if (url.pathname === '/ads.txt') {
+    return new Response('google.com, pub-4849808315998185, DIRECT, f08c47fec0942fa0\n', {
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Cache-Control': 'public, max-age=86400'
+      }
+    });
+  }
+  return context.next();
+};
